de1ba9cfac6f847273b3af8028f89a2e488192f8
Slava Nedavnii <vn1951@tbdvox.com>
2024-06-18 20:36:34 +0200

    Initial with prettier

---
+++ projects/simple/src/app.js
@@ -0,0 +1,14 @@
+const ololo = {
+    name: 'piu',
+    age: 3,
+    piu: 4,
+};
+
+const a = 4;
+const b = 5;
+const c = `ololo`;
+const arr = [1, 2, 3, 4];
+const fun = (irr) => {
+    console.log(irr);
+};
+const t = { foo: 4 };
